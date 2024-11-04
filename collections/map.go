6f5232700674f2d7bb25388ea4c368652118001f
Polshkrev <105235054+Polshkrev@users.noreply.github.com>
2024-11-04 16:19:02 -0500

    Add map constructor

--- collections/map.go
+++ collections/map.go
@@ -4,3 +4,10 @@ type Map[Key comparable, Value any] struct {
 	items map[Key]Value
 	size  uint64
 }
+
+func NewMap[Key comparable, Value any]() *Map[Key, Value] {
+	var mapping *Map[Key, Value] = new(Map[Key, Value])
+	mapping.items = make(map[Key]Value, 0)
+	mapping.size = 0
+	return mapping
+}
