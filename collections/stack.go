fea28b1becca1e679007df2366f7df632c3ba799
Polshkrev <105235054+Polshkrev@users.noreply.github.com>
2024-11-04 18:06:13 -0500

    Add stack constructor

--- collections/stack.go
+++ collections/stack.go
@@ -4,3 +4,10 @@ type Stack[Type any] struct {
 	items []Type
 	size  uint64
 }
+
+func NewStack[Type any]() *Stack[Type] {
+	var stack *Stack[Type] = new(Stack[Type])
+	stack.items = make([]Type, 0)
+	stack.size = 0
+	return stack
+}
