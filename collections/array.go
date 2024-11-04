d6c2d21cbbd480a9e74abb528932489e5137f6aa
Polshkrev <105235054+Polshkrev@users.noreply.github.com>
2024-11-04 15:38:16 -0500

    Create array constructor

--- collections/array.go
+++ collections/array.go
@@ -4,3 +4,10 @@ type Array[Type any] struct {
 	items []Type
 	size  uint64
 }
+
+func NewArray[Type any]() *Array[Type] {
+	var array *Array[Type] = new(Array[Type])
+	array.items = make([]Type, 0)
+	array.size = 0
+	return array
+}
