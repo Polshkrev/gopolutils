dfd4f5162ceca4fde034af3da49934cf4fbf4009
Polshkrev <105235054+Polshkrev@users.noreply.github.com>
2024-11-04 14:07:21 -0500

    Getters and Setters

--- version.go
+++ version.go
@@ -38,3 +38,43 @@ func NewFullVersion(name, description string, major, minor, patch uint8) *Versio
 	version.description = description
 	return version
 }
+
+func (version Version) Name() string {
+	return version.name
+}
+
+func (version Version) Description() string {
+	return version.description
+}
+
+func (version Version) Major() uint8 {
+	return version.major
+}
+
+func (version Version) Minor() uint8 {
+	return version.minor
+}
+
+func (version Version) Patch() uint8 {
+	return version.patch
+}
+
+func (version *Version) SetName(name string) {
+	version.name = name
+}
+
+func (version *Version) SetDescription(description string) {
+	version.description = description
+}
+
+func (version *Version) SetMajor(major uint8) {
+	version.major = major
+}
+
+func (version *Version) SetMinor(minor uint8) {
+	version.minor = minor
+}
+
+func (version *Version) SetPatch(patch uint8) {
+	version.patch = patch
+}
