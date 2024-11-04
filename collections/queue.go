b619b30f4d4d4a8124b08d8d966351fdf11ea647
Polshkrev <105235054+Polshkrev@users.noreply.github.com>
2024-11-04 17:33:38 -0500

    Add queue constructor

--- collections/queue.go
+++ collections/queue.go
@@ -4,3 +4,10 @@ type Queue[Type any] struct {
 	items []Type
 	size  uint64
 }
+
+func NewQueue[Type any]() *Queue[Type] {
+	var queue *Queue[Type] = new(Queue[Type])
+	queue.items = make([]Type, 0)
+	queue.size = 0
+	return queue
+}
