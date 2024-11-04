5be999d761bb8abc75487f77ef286d1443f70754
Polshkrev <105235054+Polshkrev@users.noreply.github.com>
2024-11-04 15:26:13 -0500

    Create collection interface

--- collections/collection.go
+++ collections/collection.go
@@ -1,1 +1,10 @@
 package collections
+
+import "github.com/Polshkrev/gopolutils"
+
+type Collection[Type any] interface {
+	Append(item Type)
+	Extend(items []Type)
+	At(index uint64) (*Type, *gopolutils.Exception)
+	Remove(index uint64) *gopolutils.Exception
+}
