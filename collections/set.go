ddbb42c5899b33c20202e2fcbdf980175ee20053
Polshkrev <105235054+Polshkrev@users.noreply.github.com>
2024-11-04 16:51:33 -0500

    Add set constructor

--- collections/set.go
+++ collections/set.go
@@ -4,3 +4,10 @@ type Set[Type comparable] struct {
 	items map[Type]struct{}
 	size  uint64
 }
+
+func NewSet[Type comparable]() *Set[Type] {
+	var set *Set[Type] = new(Set[Type])
+	set.items = make(map[Type]struct{}, 0)
+	set.size = 0
+	return set
+}
