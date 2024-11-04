c5e189fef4672c5f3a0bd960952b198c71be29bd
Polshkrev <105235054+Polshkrev@users.noreply.github.com>
2024-11-04 14:47:36 -0500

    Add protected exception methods

--- exception.go
+++ exception.go
@@ -1,7 +1,18 @@
 package gopolutils
 
+import "fmt"
+
 type Exception struct {
 	name    string
 	repr    string
 	message string
+}
+
+func (exception *Exception) assignRepr() {
+	exception.repr = fmt.Sprintf("%s: %s", exception.name, exception.message)
+}
+
+func (exception *Exception) assignName(name string) {
+	exception.name = name
+	exception.assignRepr()
 }
