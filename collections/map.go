04d9e5ddee5ab33d26396a1c0d9162b6ecae0658
Polshkrev <105235054+Polshkrev@users.noreply.github.com>
2024-11-04 16:23:10 -0500

    Add map insert method

--- collections/map.go
+++ collections/map.go
@@ -1,5 +1,11 @@
 package collections
 
+import (
+	"fmt"
+
+	"github.com/Polshkrev/gopolutils"
+)
+
 type Map[Key comparable, Value any] struct {
 	items map[Key]Value
 	size  uint64
@@ -10,4 +16,16 @@ func NewMap[Key comparable, Value any]() *Map[Key, Value] {
 	mapping.items = make(map[Key]Value, 0)
 	mapping.size = 0
 	return mapping
+}
+
+func (mapping *Map[Key, Value]) Insert(key Key, value Value) *gopolutils.Exception {
+	var foundException *gopolutils.Exception = gopolutils.NewNamedException("KeyError", fmt.Sprintf("Key %v already exists.", key))
+	var found bool
+	_, found = mapping.items[key]
+	if found {
+		return foundException
+	}
+	mapping.items[key] = value
+	mapping.size++
+	return nil
 }
