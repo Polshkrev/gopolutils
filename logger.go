472bc5f594f194e9f4ed59973da6b1b075a3e9c9
Polshkrev <105235054+Polshkrev@users.noreply.github.com>
2024-11-14 21:29:30 -0500

    Add logger add console method

--- logger.go
+++ logger.go
@@ -1,6 +1,16 @@
 package gopolutils
 
-import "os"
+import (
+	"os"
+)
+
+const (
+	AVAILABLE_OUTPUTS uint8 = 2
+)
+
+var (
+	OUTPUT_COUNT uint8 = 0
+)
 
 type LoggingLevel = uint8
 
@@ -40,4 +50,17 @@ func NewLogger(name string, level LoggingLevel) *Logger {
 	logger.level = level
 	logger.outputs = make([]*os.File, 0)
 	return logger
+}
+
+func (logger *Logger) append(output *os.File) {
+	logger.outputs = append(logger.outputs, output)
+}
+
+func (logger *Logger) AddConsole() *Exception {
+	if OUTPUT_COUNT >= AVAILABLE_OUTPUTS {
+		return NewNamedException("ValueError", "The number of outputs has exceeded the maximum allowed.")
+	}
+	logger.append(os.Stdout)
+	OUTPUT_COUNT++
+	return nil
 }
