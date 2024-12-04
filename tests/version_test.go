469724274c2acb85daf6b8f6ed9379198e193632
Polshkrev <105235054+Polshkrev@users.noreply.github.com>
2024-12-04 11:54:05 -0500

    Create version_test.go

---
+++ tests/version_test.go
@@ -0,0 +1,18 @@
+package tests
+
+import (
+	"testing"
+
+	"github.com/Polshkrev/gopolutils"
+)
+
+func TestNewVersionZerosOut(test *testing.T) {
+	var version *gopolutils.Version = gopolutils.NewVersion()
+	if version.Major() != 0 {
+		test.Errorf("New version major is not set correctly: %d. Expected: %d\n", version.Major(), 0)
+	} else if version.Minor() != 0 {
+		test.Errorf("New version minor is not set correctly: %d. Expected: %d\n", version.Minor(), 0)
+	} else if version.Patch() != 0 {
+		test.Errorf("New version patch is not set correctly: %d. Expected: %d\n", version.Patch(), 0)
+	}
+}
