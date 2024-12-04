0570d320b98f1ab957345d61a03643fa721d5072
Polshkrev <105235054+Polshkrev@users.noreply.github.com>
2024-12-04 12:03:06 -0500

    Add pair constructor

--- collections/pair.go
+++ collections/pair.go
@@ -4,3 +4,10 @@ type Pair[First any, Second any] struct {
 	first  First
 	second Second
 }
+
+func NewPair[First any, Second any](first First, second Second) *Pair[First, Second] {
+	var pair *Pair[First, Second] = new(Pair[First, Second])
+	pair.first = first
+	pair.second = second
+	return pair
+}
