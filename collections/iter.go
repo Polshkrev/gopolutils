29495fc51fca2fcd785c5f595c47e75dff2ac5a7
Polshkrev <105235054+Polshkrev@users.noreply.github.com>
2024-12-05 21:11:09 -0500

    Update set
    Set now implements the collection, view, and sized interfaces.

--- collections/iter.go
+++ collections/iter.go
@@ -2,11 +2,11 @@ package collections
 
 // Implementation of a python-like enumeration function iterator.
 // Returns an enumerated function iterator.
-func Enumerate[Type any](items []Type) func(func(uint64, Type) bool) {
+func Enumerate[Type any](items View[Type]) func(func(uint64, Type) bool) {
 	return func(yield func(uint64, Type) bool) {
 		var i uint64
-		for i = 0; i <= uint64(len(items)-1); i++ {
-			if !yield(i, items[i]) {
+		for i = 0; i <= items.Size()-1; i++ {
+			if !yield(i, items.Collect()[i]) {
 				return
 			}
 		}
@@ -15,9 +15,9 @@ func Enumerate[Type any](items []Type) func(func(uint64, Type) bool) {
 
 // Determine if a given item is within a given slice of items.
 // Returns true if the given item is present within the given slice of items.
-func In[Type comparable](items []Type, item Type) bool {
+func In[Type comparable](items View[Type], item Type) bool {
 	var i Type
-	for _, i = range items {
+	for _, i = range items.Collect() {
 		if i != item {
 			continue
 		}
