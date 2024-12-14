0cbd8657555d66939729f369c4737ccb86d401a2
Polshkrev <105235054+Polshkrev@users.noreply.github.com>
2024-12-14 14:33:08 -0500

    Add version covert test

--- tests/version_test.go
+++ tests/version_test.go
@@ -16,3 +16,14 @@ func TestNewVersionZerosOut(test *testing.T) {
 		test.Errorf("New version patch is not set correctly: %d. Expected: %d\n", version.Patch(), 0)
 	}
 }
+
+func TestVersionConvertSetsCorrectly(test *testing.T) {
+	var version *gopolutils.Version = gopolutils.VersionConvert(1, 1, 1)
+	if version.Major() != 1 {
+		test.Errorf("New version major is not set correctly: %d. Expected: %d\n", version.Major(), 0)
+	} else if version.Minor() != 1 {
+		test.Errorf("New version minor is not set correctly: %d. Expected: %d\n", version.Minor(), 0)
+	} else if version.Patch() != 1 {
+		test.Errorf("New version patch is not set correctly: %d. Expected: %d\n", version.Patch(), 1)
+	}
+}
