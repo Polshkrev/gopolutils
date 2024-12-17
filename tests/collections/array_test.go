9f0f439f03ad689ba6c6be3b4cd8a716abf4d7ed
Polshkrev <105235054+Polshkrev@users.noreply.github.com>
2024-12-17 18:35:36 -0500

    Update array_test.go

--- tests/collections/array_test.go
+++ tests/collections/array_test.go
@@ -6,17 +6,6 @@ import (
 	"github.com/Polshkrev/gopolutils/collections"
 )
 
-var (
-	arrayMock *collections.Array[int] = collections.NewArray[int]()
-)
-
-func setupArrayMock(testing.TB) func(testing.TB) {
-	arrayMock.Append(0)
-	arrayMock.Append(1)
-	arrayMock.Append(2)
-	return func(testing.TB) {}
-}
-
 func TestArrayConstructNotNil(test *testing.T) {
 	var nilArray *collections.Array[int] = collections.NewArray[int]()
 	if nilArray == nil {
@@ -25,48 +14,58 @@ func TestArrayConstructNotNil(test *testing.T) {
 }
 
 func TestArrayAppendSuccess(test *testing.T) {
-	var teardown func(testing.TB) = setupArrayMock(test)
-	defer teardown(test)
-	if !collections.In(arrayMock, 1) {
-		test.Errorf("Can not find '%d' in array '%+v'\n", 1, *arrayMock)
+	var mock *collections.Array[int] = collections.NewArray[int]()
+	mock.Append(0)
+	mock.Append(1)
+	mock.Append(2)
+	if !collections.In(mock, 1) {
+		test.Errorf("Can not find '%d' in array '%+v'\n", 1, *mock)
 	}
 }
 
 func TestArrayAppendFail(test *testing.T) {
-	var teardown func(testing.TB) = setupArrayMock(test)
-	defer teardown(test)
-	if collections.In(arrayMock, 10) {
-		test.Errorf("Can not find '%d' in array '%+v'\n", 10, *arrayMock)
+	var mock *collections.Array[int] = collections.NewArray[int]()
+	mock.Append(0)
+	mock.Append(1)
+	mock.Append(2)
+	if collections.In(mock, 10) {
+		test.Errorf("Can not find '%d' in array '%+v'\n", 10, *mock)
 	}
 }
 
 func TestArrayAtSuccess(test *testing.T) {
-	var teardown func(testing.TB) = setupArrayMock(test)
-	defer teardown(test)
+	var mock *collections.Array[int] = collections.NewArray[int]()
+	mock.Append(0)
+	mock.Append(1)
+	mock.Append(2)
 	var item *int
 	var except *gopolutils.Exception
-	item, except = arrayMock.At(1)
+	item, except = mock.At(1)
 	if *item != 1 || except != nil{
-		test.Errorf("Can not find '%d' in array '%+v'. %s\n", 1, *arrayMock, except.Error())
+		test.Errorf("Can not find '%d' in array '%+v'. %s\n", 1, *mock, except.Error())
 	}
 }
 
 func TestArrayAtFail(test *testing.T) {
-	var teardown func(testing.TB) = setupArrayMock(test)
-	defer teardown(test)
+	var mock *collections.Array[int] = collections.NewArray[int]()
+	mock.Append(0)
+	mock.Append(1)
+	mock.Append(2)
 	var item *int
 	var except *gopolutils.Exception
-	item, except = arrayMock.At(20)
+	item, except = mock.At(20)
 	if except == nil {
-		test.Errorf("Except at index '%d' is nil in array '%+v' with value '%d'.\n", 20, *arrayMock, *item)
+		test.Errorf("Except at index '%d' is nil in array '%+v' with value '%d'.\n", 20, *mock, *item)
 	}
 }
 
 func TestArrayRemoveSuccess(test *testing.T) {
-	var teardown func(testing.TB) = setupArrayMock(test)
-	defer teardown(test)
-	var except *gopolutils.Exception = arrayMock.Remove(1)
-	if collections.In(arrayMock, 1) {
-		test.Errorf("'%d' was not removed from array '%+v'.\n", 1, *arrayMock)
+	var mock *collections.Array[int] = collections.NewArray[int]()
+	mock.Append(0)
+	mock.Append(1)
+	mock.Append(2)
+	var _ *gopolutils.Exception = mock.Remove(1)
+	if collections.In(mock, 1) {
+		test.Errorf("'%d' was not removed from array '%+v'.\n", 1, *mock)
 	}
 }
