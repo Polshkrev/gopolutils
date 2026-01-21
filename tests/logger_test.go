793a4b54938bdb5a73cade177f50958090720cd9
Polshkrev <105235054+Polshkrev@users.noreply.github.com>
2026-01-20 20:57:10 -0500

    Update logger_test.go

--- tests/logger_test.go
+++ tests/logger_test.go
@@ -8,7 +8,7 @@ import (
 
 // This functionality is not implemented yet. This test should fail.
 func TestGetLevelSuccess(test *testing.T) {
-	var level gopolutils.LoggingLevel = gopolutils.DEBUG
+	var level gopolutils.LoggingLevel = gopolutils.Debug
 	var logger *gopolutils.Logger = gopolutils.NewLogger("test", level)
 	var result gopolutils.LoggingLevel = logger.GetLevel()
 	if result != level {
@@ -18,19 +18,19 @@ func TestGetLevelSuccess(test *testing.T) {
 
 // This functionality is not implemented yet. This test should fail.
 func TestSetLevelSuccess(test *testing.T) {
-	var logger *gopolutils.Logger = gopolutils.NewLogger("test", gopolutils.DEBUG)
-	logger.SetLevel(gopolutils.INFO)
+	var logger *gopolutils.Logger = gopolutils.NewLogger("test", gopolutils.Debug)
+	logger.SetLevel(gopolutils.Info)
 	var result gopolutils.LoggingLevel = logger.GetLevel()
-	if result != gopolutils.INFO {
-		test.Errorf("Expected: %d got: %d", gopolutils.INFO, result)
+	if result != gopolutils.Info {
+		test.Errorf("Expected: %d got: %d", gopolutils.Info, result)
 	}
 }
 
 func TestSetLevelFromSameLevel(test *testing.T) {
-	var logger *gopolutils.Logger = gopolutils.NewLogger("test", gopolutils.DEBUG)
-	logger.SetLevel(gopolutils.DEBUG)
+	var logger *gopolutils.Logger = gopolutils.NewLogger("test", gopolutils.Debug)
+	logger.SetLevel(gopolutils.Debug)
 	var result gopolutils.LoggingLevel = logger.GetLevel()
-	if result != gopolutils.DEBUG {
-		test.Errorf("Expected: %d got: %d", gopolutils.DEBUG, result)
+	if result != gopolutils.Debug {
+		test.Errorf("Expected: %d got: %d", gopolutils.Debug, result)
 	}
 }
