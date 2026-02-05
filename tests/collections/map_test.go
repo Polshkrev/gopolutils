4ae679b331bc9fcc3de9fd1858d7eb8f5aae6b16
Polshkrev <105235054+Polshkrev@users.noreply.github.com>
2026-02-05 13:26:46 -0500

    Update map_test.go

--- tests/collections/map_test.go
+++ tests/collections/map_test.go
@@ -25,7 +25,7 @@ func TestMapInsertSuccess(test *testing.T) {
 func TestMapInsertFail(test *testing.T) {
 	var mapping *collections.Map[int, int] = collections.NewMap[int, int]()
 	var except *gopolutils.Exception = mapping.Insert(0, 0)
-	except = mapping.Insert(0, 0)
+	except = mapping.Insert(0, 1)
 	if except == nil {
 		test.Errorf("Map insert returned nil: %+v.", *mapping)
 	}
