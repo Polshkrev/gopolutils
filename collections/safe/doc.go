bf67c5cec6a1ea795f7e5f1a1bfb14efe21ac695
Polshkrev <105235054+Polshkrev@users.noreply.github.com>
2026-02-11 17:34:15 -0500

    Add safe/doc.go

---
+++ collections/safe/doc.go
@@ -0,0 +1,16 @@
+/*
+Safe provides interfaces of standardization for operations related to concurrent safe data structures.
+Standardizations defined within the collections package include:
+
+Interfaces:
+
+  - [Collection]: An interface for linear data structures.
+  - [Mapping]: An interface for key-value pairs.
+
+Implementations:
+  - [Array]
+  - [Map]
+  - [Queue]
+  - [Stack]
+*/
+package collections
