7263e29306ff3d6c7a0e06f891a485613c1dd58a
Polshkrev <105235054+Polshkrev@users.noreply.github.com>
2026-02-11 17:24:44 -0500

    Update collection.go

--- collections/safe/collection.go
+++ collections/safe/collection.go
@@ -3,7 +3,7 @@ package safe
 import "github.com/Polshkrev/gopolutils/collections"
 
 // Representation of a collection of with an internal lock.
-type SafeCollection[Type any] interface {
+type Collection[Type any] interface {
 	// Lock the internal mutex of the collection for both reading and writing.
 	Lock()
 	// Unlock the internal mutex of the collection for both reading and writing.
