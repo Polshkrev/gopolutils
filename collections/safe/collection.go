7a07a1ec1f1eb59608313507cbefd52d8682b9e2
Polshkrev <105235054+Polshkrev@users.noreply.github.com>
2026-03-06 04:45:01 -0500

    Embed lockable and unlockable

--- collections/safe/collection.go
+++ collections/safe/collection.go
@@ -4,13 +4,7 @@ import "github.com/Polshkrev/gopolutils/collections"
 
 // Representation of a collection of with an internal lock.
 type Collection[Type any] interface {
-	// Lock the internal mutex of the collection for both reading and writing.
-	Lock()
-	// Unlock the internal mutex of the collection for both reading and writing.
-	Unlock()
-	// Lock the internal mutex of the collection for reading.
-	RLock()
-	// Unock the internal mutex of the collection for reading.
-	RUnlock()
+	Lockable
+	Unlockable
 	collections.Collection[Type]
 }
--- collections/safe/mapping.go
+++ collections/safe/mapping.go
@@ -3,13 +3,7 @@ package safe
 import "github.com/Polshkrev/gopolutils/collections"
 
 type Mapping[Key, Value any] interface {
-	// Lock the internal mutex of the mapping for both reading and writing.
-	Lock()
-	// Unlock the internal mutex of the mapping for both reading and writing.
-	Unlock()
-	// Lock the internal mutex of the mapping for reading.
-	RLock()
-	// Unock the internal mutex of the mapping for reading.
-	RUnlock()
+	Lockable
+	Unlockable
 	collections.Mapping[Key, Value]
 }
