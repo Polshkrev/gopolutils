88a7c8ce961acde98f9fa7bc4b1ad9accb3e9abe
Polshkrev <105235054+Polshkrev@users.noreply.github.com>
2026-07-31 13:50:32 -0400

    Add None and Callback types

--- logger.go
+++ logger.go
@@ -42,8 +42,6 @@ const (
 // If the logging level is not defined in the enum, an [UnreachableError] is returned.
 func (level LoggingLevel) String() string {
 	switch level {
-	case Debug:
-		return "DEBUG"
 	case Info:
 		return "INFO"
 	case Warning:
@@ -52,8 +50,9 @@ func (level LoggingLevel) String() string {
 		return "ERROR"
 	case Critical:
 		return "CRITICAL"
+	default:
+		return "DEBUG"
 	}
-	return NewNamedException(UnreachableError, "Unknown logging level. %v", level).Error() // unreachable
 }
 
 // A logger.
