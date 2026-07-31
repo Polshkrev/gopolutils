b3e55ef84f0130b7f6a557eb77e81b1835fc6ac9
Polshkrev <105235054+Polshkrev@users.noreply.github.com>
2026-07-31 13:54:06 -0400

    Update set.go

--- collections/set.go
+++ collections/set.go
@@ -13,14 +13,14 @@ var _ Iterable[any] = (*Set[any])(nil)
 
 // Implementation of a set.
 type Set[Type comparable] struct {
-	items Mapping[Type, None]
+	items Mapping[Type, gopolutils.None]
 }
 
 // Construct a new set.
 // Returns a pointer to a new empty set.
 func NewSet[Type comparable]() *Set[Type] {
 	var set *Set[Type] = new(Set[Type])
-	set.items = NewMap[Type, None]()
+	set.items = NewMap[Type, gopolutils.None]()
 	return set
 }
 
@@ -30,7 +30,7 @@ func (set *Set[Type]) Append(item Type) {
 	if set.Contains(item) {
 		return
 	}
-	var except *gopolutils.Exception = set.items.Insert(item, None{})
+	var except *gopolutils.Exception = set.items.Insert(item, gopolutils.None{})
 	if except != nil {
 		fmt.Fprintln(os.Stderr, except.Error())
 		os.Exit(1)
